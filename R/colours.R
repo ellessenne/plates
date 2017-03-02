ab201eb67af87ef74a2398306829cb31d32bb029
Alessandro Gasparini <ag475@le.ac.uk>
2017-03-02 17:02:22 +0000

    Included list of colours according to guidelines

---
+++ R/colours.R
@@ -0,0 +1,20 @@
+#' @title Complete list of palettes
+#'
+#' @description Use the functions \code{\link{get_uol_palette}}, \code{\link{get_uol_palette_mono}}, \code{\link{get_uol_palette_manual}} to obtain the desired color palette.
+#' Plase note that \code{blue}, \code{grey}, and \code{brown} are all primary colours, while the remaining are secondary ones.
+#' @return A list of colors, in HEX format.
+#'
+#' @export
+
+uol_colours <- list(
+  blue = c("#004366", "#00679E", "#C3D0E1"),
+  grey = c("#667177", "#969A9D", "#CDD0CF"),
+  brown = c("#6C4D56", "#926A74", "#C5AEB1"),
+  red = c("#C71F4B", "#DE6A70", "#EFD3DB"),
+  green = c("#00A783", "#6CBD9B", "#B2D8C4"),
+  brightgreen = c("#B3C50E", "#D4DC89", "#E4E7B5"),
+  orange = c("#E27F20", "#E79947", "#F1CCA3"),
+  lightblue = c("#009BC8", "#00AFD4", "#BCDDE9"),
+  purple = c("#832672", "#A5649C", "#D7C6DA"),
+  yellow = c("#DFCB00", "#EADB75", "#F2ECC3"),
+  pink = c("#D60B7B", "#DE76A4", "#EECEDC"))
