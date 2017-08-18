57208b47f64a40aa888771d3d2f85ebc06e32283
Alessandro Gasparini <ag475@le.ac.uk>
2017-08-18 12:14:41 +0100

    Improved template and documentation for Remark.js template

--- R/uol_remarkjs.R
+++ R/uol_remarkjs.R
@@ -1,12 +1,13 @@
 #' A minimal Remark.js template following University of leicester visual identity guidelines
 #'
-#' Write something here...
+#' This format is a wrapper around \code{\link[xaringan]{moon_reader}}. It includes a custom .css file following visual identity guidelines issued by the University of Leicester.
 #'
 #' @inheritParams xaringan::moon_reader
-#' @param ... Additional arguments to \code{xaringan::moon_reader}
+#' @param ... Additional arguments passed to \code{\link[xaringan]{moon_reader}}
 #'
-#' @return R Markdown output format to pass to \code{\link[rmarkdown:render]{render}}
+#' @note Do not trust the preview of the presentation from the RStudio viewer or window. Instead, open the presentation with a web browser and refresh to see changes.
 #'
+#' @note Include \code{css: ["default", "uol_remarkjs"]} in the YAML header to include the standard \code{xaringan} CSS file. For instance, that improves printing to PDF using Google Chrome. More details on printing to PDF in \href{https://github.com/yihui/xaringan/issues/65}{#65} and \href{https://github.com/yihui/xaringan/wiki/Export-Slides-to-PDF}{here}.
 #' @examples
 #'
 #' \dontrun{
@@ -15,7 +16,7 @@
 #' }
 #'
 #' @export
-uol_remarkjs <- function(..., css = "default") {
-  if ("default" %in% css) css = c(setdiff(css, "default"), find_resource("uol_remarkjs", "uol_remarkjs.css"))
+uol_remarkjs <- function(..., css = "uol_remarkjs") {
+  if ("uol_remarkjs" %in% css) css = c(setdiff(css, "uol_remarkjs"), find_resource("uol_remarkjs", "uol_remarkjs.css"))
   xaringan::moon_reader(css = css, ...)
 }
