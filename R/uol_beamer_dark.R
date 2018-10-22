f3f7afc9d39103cfe2d8b8517101e7d92e752878
Alessandro Gasparini <ag475@leicester.ac.uk>
2018-10-22 15:44:27 +0100

    Added dark beamer theme

---
+++ R/uol_beamer_dark.R
@@ -0,0 +1,25 @@
+#' A minimal, dark beamer template following University of Leicester visual identity guidelines
+#'
+#' @description A beamer template following University of Leicester visual identity guidelines
+#' @inheritParams rmarkdown::beamer_presentation
+#' @param ... Additional arguments to [rmarkdown::beamer_presentation()]
+#'
+#' @return R Markdown output format to pass to [rmarkdown::render()]
+#'
+#' @examples
+#' 
+#' \dontrun{
+#' library(rmarkdown)
+#' draft("my_presentation.Rmd", template = "uol_beamer_dark", package = "uolvid")
+#' }
+#' 
+#' @export
+uol_beamer_dark <- function(...) {
+  rmarkdown::beamer_presentation(
+    ...,
+    template = find_resource("uol_beamer_dark", "template.tex"),
+    keep_tex = TRUE,
+    highlight = NULL,
+    latex_engine = "xelatex"
+  )
+}
