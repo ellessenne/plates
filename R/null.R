1a11addb586e5df45e070d8aa26e0f6082a50926
Alessandro Gasparini <ellessenne@gmail.com>
2019-07-30 15:32:58 +0100

    Added beamertheme-null

---
+++ R/null.R
@@ -0,0 +1,25 @@
+#' A streamlined beamer template based on the default theme
+#'
+#' @description A streamlined beamer template absed on the default theme
+#' @inheritParams rmarkdown::beamer_presentation
+#' @param ... Additional arguments to [rmarkdown::beamer_presentation()]
+#'
+#' @return R Markdown output format to pass to [rmarkdown::render()]
+#'
+#' @examples
+#'
+#' \dontrun{
+#' library(rmarkdown)
+#' draft("my_presentation.Rmd", template = "null", package = "uolvid")
+#' }
+#'
+#' @export
+null <- function(...) {
+  rmarkdown::beamer_presentation(
+    ...,
+    template = find_resource("null", "template.tex"),
+    keep_tex = TRUE,
+    highlight = NULL,
+    latex_engine = "pdflatex"
+  )
+}
